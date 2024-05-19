02a8db07eb0840da74bafe41a6d8da2cd6bd6a35
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2024-05-19 23:48:04 +0200

    feat: remove type from Mot class

--- src/main/java/com/antonio/apprendrebackend/controller/MotController.java
+++ src/main/java/com/antonio/apprendrebackend/controller/MotController.java
@@ -1,15 +1,17 @@
 package com.antonio.apprendrebackend.controller;
 
+import com.antonio.apprendrebackend.model.ErrorResponse;
 import com.antonio.apprendrebackend.model.Mot;
-import com.antonio.apprendrebackend.model.Type;
+import com.antonio.apprendrebackend.model.Palabra;
 import com.antonio.apprendrebackend.repository.MotRepository;
+import com.antonio.apprendrebackend.util.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/mot")
@@ -18,15 +20,31 @@ public class MotController {
     private MotRepository motRepository;
 
     @PostMapping(path = "/add")
-    public @ResponseBody Mot addNewMot(@RequestParam String name
-            , @RequestParam Type type) {
-        Mot mot = new Mot(name, type);
+    public @ResponseBody ResponseEntity<?> addNewMot(@RequestParam String name) {
+        Optional<Mot> existingMot = motRepository.findByName(name);
+        if (existingMot.isPresent()) {
+            ErrorResponse errorResponse = new ErrorResponse("El Mot ya existe", ErrorCode.MOT_EXISTS);
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
 
-        return motRepository.save(mot);
+        Mot mot = new Mot(name);
+        motRepository.save(mot);
+
+        return new ResponseEntity<>(mot, HttpStatus.CREATED);
     }
 
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Mot> getAllMots() {
+    @GetMapping(path = "/getByName")
+    public @ResponseBody Optional<Mot> getMotByName(@RequestParam String name) {
+        return motRepository.findByName(name);
+    }
+
+    @GetMapping(path = "/getById")
+    public @ResponseBody Optional<Mot> getMotById(@RequestParam Integer motId) {
+        return motRepository.findById(motId);
+    }
+
+    @GetMapping(path = "/getAll")
+    public @ResponseBody Iterable<Mot> getAllMot() {
         return motRepository.findAll();
     }
 }
--- src/main/java/com/antonio/apprendrebackend/model/Mot.java
+++ src/main/java/com/antonio/apprendrebackend/model/Mot.java
@@ -10,13 +10,8 @@ public class Mot {
 
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "type_id", nullable = false)
-    private Type type;
-
-    public Mot(String name, Type type) {
+    public Mot(String name) {
         this.name = name;
-        this.type = type;
     }
 
     public Mot() {
@@ -38,20 +33,12 @@ public class Mot {
         this.name = name;
     }
 
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
 
     @Override
     public String toString() {
         return "Mot{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", type=" + type +
                 '}';
     }
 }
