4d4676646e1fb09556320d7bebd37bb090fa6a45
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-01-30 22:40:36 +0100

    :bulb: Add comment to phrasesWithWordTranslations method

--- src/main/java/com/antonio/apprendrebackend/service/controller/PhraseController.java
+++ src/main/java/com/antonio/apprendrebackend/service/controller/PhraseController.java
@@ -2,6 +2,7 @@ package com.antonio.apprendrebackend.service.controller;
 
 import com.antonio.apprendrebackend.service.dto.PhraseWithWordTranslationsDTO;
 import com.antonio.apprendrebackend.service.dto.WordTranslationDTO;
+import com.antonio.apprendrebackend.service.exception.PhraseNotFoundException;
 import com.antonio.apprendrebackend.service.service.PhraseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,15 @@ public class PhraseController {
     @Autowired
     PhraseService phraseService;
 
+    /**
+     * Get All Phrases and their WordTranslation of a deck
+     *
+     * @param deckId
+     * @return HTTP respond with a List<PhraseWithWordTranslationsDTO>>
+     * @throws PhraseNotFoundException if not exist any Phrase of the deck
+     */
     @GetMapping(path = "{deckId}")
-    public @ResponseBody ResponseEntity<?> getAllPhrasesWithWordTranslationsByDeck(@PathVariable Integer deckId) {
+    public @ResponseBody ResponseEntity<List<PhraseWithWordTranslationsDTO>> getAllPhrasesWithWordTranslationsByDeck(@PathVariable Integer deckId) {
         List<PhraseWithWordTranslationsDTO> phrasesWithWordTranslations = phraseService.getAllPhrasesWithWordTranslationsByDeck(deckId);
         return ResponseEntity.ok(phrasesWithWordTranslations);
     }
--- src/main/java/com/antonio/apprendrebackend/service/service/PhraseService.java
+++ src/main/java/com/antonio/apprendrebackend/service/service/PhraseService.java
@@ -1,6 +1,7 @@
 package com.antonio.apprendrebackend.service.service;
 
 import com.antonio.apprendrebackend.service.dto.PhraseWithWordTranslationsDTO;
+import com.antonio.apprendrebackend.service.exception.PhraseNotFoundException;
 import com.antonio.apprendrebackend.service.model.Phrase;
 import com.antonio.apprendrebackend.service.model.WordTranslation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,5 +12,12 @@ import java.util.Optional;
 public interface PhraseService {
     List<Phrase> findPhrasesByWordTranslation(WordTranslation wordTranslation);
 
+    /**
+     * Get All Phrases and their WordTranslation of a deck
+     *
+     * @param deckId
+     * @return HTTP respond with a List<PhraseWithWordTranslationsDTO>>
+     * @throws PhraseNotFoundException if not exist any Phrase of the deck
+     */
     List<PhraseWithWordTranslationsDTO> getAllPhrasesWithWordTranslationsByDeck(Integer deckId);
 }
