d2dfaae16fa55579de153bbdf167d4bd5eae0d75
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-03-02 23:51:05 +0100

    :ambulance: add allowed origin in order to allow supabase

--- src/main/java/com/antonio/apprendrebackend/service/config/WebConfig.java
+++ src/main/java/com/antonio/apprendrebackend/service/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://apprendre-backend.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
