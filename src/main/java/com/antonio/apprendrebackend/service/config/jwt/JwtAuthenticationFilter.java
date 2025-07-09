83f81c81921b40910e643d8c08d196f2c432f934
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-07-09 15:44:12 +0200

    :ambulance: Added application-prod.properties removed after v0.3.0 version

--- src/main/java/com/antonio/apprendrebackend/service/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/antonio/apprendrebackend/service/config/jwt/JwtAuthenticationFilter.java
@@ -6,8 +6,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,11 +17,14 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    @Autowired
-    JwtService jwtService;
+    private final JwtService jwtService;
     private final UserInfoService userInfoService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserInfoService userInfoService) {
+        this.jwtService = jwtService;
+        this.userInfoService = userInfoService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
