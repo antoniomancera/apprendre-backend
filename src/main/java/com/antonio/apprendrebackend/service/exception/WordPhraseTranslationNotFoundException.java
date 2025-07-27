1b58aa7a92bf25af377095fdab38788ff5380e38
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-07-27 22:15:46 +0200

    :goal_net: feature: add cath error for WordPhraseTranslationNotFound, andDeckAlreadyExists

---
+++ src/main/java/com/antonio/apprendrebackend/service/exception/WordPhraseTranslationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.antonio.apprendrebackend.service.exception;
+
+public class WordPhraseTranslationNotFoundException extends RuntimeException {
+    public WordPhraseTranslationNotFoundException(String message) {
+        super(message);
+    }
+}
