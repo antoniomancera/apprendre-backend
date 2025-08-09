72f885fc6e6bfd72a5d52f525243e5c21a13c68a
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-08-09 12:47:19 +0200

    :truck: refactor: rename the column level of table level to name to name and other method related to him

--- src/main/java/com/antonio/apprendrebackend/service/model/Level.java
+++ src/main/java/com/antonio/apprendrebackend/service/model/Level.java
@@ -1,10 +1,16 @@
 package com.antonio.apprendrebackend.service.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Level {
     public enum LevelEnum {
         A1, A2, B1, B2, C1, C2
@@ -15,9 +21,9 @@ public class Level {
     private Integer id;
 
     @Enumerated(EnumType.STRING)
-    private LevelEnum level;
+    private LevelEnum name;
 
-    public Level(LevelEnum level) {
-        this.level = level;
+    public Level(LevelEnum name) {
+        this.name = name;
     }
 }
