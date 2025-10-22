397c291170df2063ae515a654b65ec8d70ec7414
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-10-22 11:49:32 +0200

    :wastebasket: feat: added comments for deprecated code

--- src/main/java/com/antonio/apprendrebackend/service/dto/ConjugationRegularIrregularDTO.java
+++ src/main/java/com/antonio/apprendrebackend/service/dto/ConjugationRegularIrregularDTO.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+//TODO eliominar?
 @NoArgsConstructor
 @Getter
 @Setter
--- src/main/java/com/antonio/apprendrebackend/service/dto/ConjugationTenseDTO.java
+++ src/main/java/com/antonio/apprendrebackend/service/dto/ConjugationTenseDTO.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 import java.util.Map;
 
+//TOOD eliominar?
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
